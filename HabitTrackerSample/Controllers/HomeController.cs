f94ffe6f80381fe3b6128c4e52e277182e33018f
Buddy Favors Jr <buddy.favors@skyepack.com>
2024-05-09 17:47:26 -0400

    Add project files.

---
+++ HabitTrackerSample/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HabitTrackerSample.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("/")]
+        public string Get()
+        {
+            return "Hello and welcome to your C# and API test project!";
+        }
+    }
+}
