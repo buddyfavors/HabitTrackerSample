3780ebd864a1c52346964ae9884b90e7a5bce0c8
Buddy Favors Jr <buddy.favors@skyepack.com>
2024-05-09 18:19:45 -0400

    Basic EF setup for testing. Added habit table and quick test to create a habit and query the count.

--- HabitTrackerSample/Controllers/HomeController.cs
+++ HabitTrackerSample/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HabitTrackerSample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HabitTrackerSample.Controllers
 {
@@ -8,7 +10,34 @@ namespace HabitTrackerSample.Controllers
         [HttpGet("/")]
         public string Get()
         {
-            return "Hello and welcome to your C# and API test project!";
+            return $"Hello and welcome to your C# and API test project! This project will servve as a quick and dirty test, it is not expetected to take much time and mimicing existing work in different ways.";
+        }
+
+        [HttpGet("/CreateHabit")]
+        public async Task<string> CreateHabit()
+        {
+            using var db = new HabitTrackerContext();
+
+            var habit = new Habit
+            {
+                Name = Guid.NewGuid().ToString(),
+            };
+
+            db.Habits.Add(habit);
+
+            await db.SaveChangesAsync();
+
+            return $"HabitId: {habit.HabitId}";
+        }
+
+        [HttpGet("/GetHabitCount")]
+        public async Task<string> GetHabitCount()
+        {
+            using var db = new HabitTrackerContext();
+
+            var habitCount = await db.Habits.CountAsync();
+
+            return $"Habit Count: {habitCount}";
         }
     }
 }
---
+++ HabitTrackerSample/Models/Habit.cs
@@ -0,0 +1,8 @@
+﻿namespace HabitTrackerSample.Models
+{
+    public class Habit
+    {
+        public int HabitId { get; set; }
+        public string Name { get; set; }
+    }
+}
