66e0572f28c99c90da26edd87690650266a87f43
Chris Moe <cjmoe@iu.edu>
2024-05-13 15:50:39 -0400

    Fixed missing files

---
+++ HabitTrackerSample/Models/Rewards.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace HabitTrackerSample.Models
+{
+    public class Rewards
+    {
+        [Key]
+        public int RewardId { get; set; }
+        public string Name { get; set;}
+        public DateTime Created {  get; set; }
+    }
+}
